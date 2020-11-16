06c4a00f7d5556013889451ddacbe6480289e640
ken.baum <ilpadre1953@gmail.com>
2020-11-16 08:07:12 -0500

    Experiment with container-fluid

--- src/app/components/app-container/scrumptious-agile-app/scrumptious-agile/scrumptious-agile-dashboard/scrumptious-agile-dashboard.component.ts
+++ src/app/components/app-container/scrumptious-agile-app/scrumptious-agile/scrumptious-agile-dashboard/scrumptious-agile-dashboard.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ScrumptiousAgileDashboardComponent implements OnInit {
 
   constructor() { }
-
+  title = 'Scrumptious in the Dashboard';
   ngOnInit(): void {
   }
 
